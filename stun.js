fbe4c91db09b70f7823856f07f41468aee418f4c
Alain Iranzi <alainiranzi00@gmail.com>
2025-10-01 02:02:11 +0200

    sections with other interactions

--- stun.js
+++ stun.js
@@ -1,7 +1,6 @@
 document.getElementById("reserveBtnHero").addEventListener("click", function() {
     window.open("https://stung-upon-gap.figma.site/classes", "_blank");
   });
-// Scroll Fade-in Animation
 const sections = document.querySelectorAll('.fade-in');
 
 const observer = new IntersectionObserver(entries => {
@@ -16,18 +15,18 @@ const observer = new IntersectionObserver(entries => {
 
 sections.forEach(section => observer.observe(section));
 
-// Scroll to Section Buttons
+
 function scrollToSection(id) {
   const section = document.getElementById(id);
   section.scrollIntoView({ behavior: 'smooth' });
 }
 
-// Header Motion Animation on Scroll
+
 const navLinks = document.querySelector('.nav-links');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
-    navLinks.style.transform = "translateX(10px)"; // items iburyo bimove gahoro
+    navLinks.style.transform = "translateX(10px)";
   } else {
     navLinks.style.transform = "translateX(0)";
   }
@@ -36,9 +35,9 @@ const text = document.getElementById('scrollingText');
 let translateX = 0;
 
 function animateText() {
-  translateX -= 1; // uko igenda gahoro cyangwa vuba
+  translateX -= 1; 
 
-  // Iyo text yose imaze kuva ibumoso, subira iburyo
+  
   if(-translateX > text.offsetWidth) {
     translateX = window.innerWidth;
   }
@@ -49,7 +48,7 @@ function animateText() {
 
 animateText();
 document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.banner, .sectiontwo-wrapper, .sectionthree-wrapper, .founders-section');
+  const sections = document.querySelectorAll('.banner, .sectiontwo-wrapper, .sectionthree-wrapper, .founders-section, .dembele, .footer, .footer-bottom ');
   window.addEventListener('scroll', () => {
     const scrollTop = window.scrollY;
   
@@ -57,7 +56,9 @@ document.addEventListener('DOMContentLoaded', () => {
     sections.forEach(section => {
       section.style.transform = `translateY(${Math.max(-scrollTop, -document.querySelector('.hero').offsetHeight)}px)`;
 
-    });
+    });document.getElementById("youtubeBtn").addEventListener("click", function() {
+  window.open("https://www.youtube.com/@AlainIranzi", "_blank");
+});
   });
 });
 
